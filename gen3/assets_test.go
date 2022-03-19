03e801505c1abf4d5bdfeb7bf06dce7d1e1b6857
Andrea Jemmett <1787979+acidghost@users.noreply.github.com>
2022-03-19 12:11:29 +0100

    Fix support fresh saves with HoF uninit sectors
    Before entering the HoF for the first time, the related sectors are
    untouched by the game and hence remain unitialized from NAND memory
    which sets it to 0xFF.
    
    Save files that have gone through PKHeX have still the HoF sectors all
    set to 0xFF, except for the checksum field which is computed as 0xFC1F
    from the 0xFF data portion. This means that they will also have the
    security field set to 0xFFFFFFFF (invalid).
    
    After entering the HoF for the first time, the second sector will be
    semantically empty, but will have the data portion set to 0x00, valid
    checksum (incidentally?) 0x00 and valid security field.

---
+++ gen3/assets_test.go
@@ -0,0 +1,14 @@
+package gen3
+
+import _ "embed"
+
+//go:embed assets_test/e_fresh_save_B.sav
+var eFreshSaveB []byte
+//go:embed assets_test/e_fresh_save_AB.sav
+var eFreshSaveAB []byte
+//go:embed assets_test/e_hof0.sav
+var eHoF0Save []byte
+//go:embed assets_test/e_pkhex_after_starter.sav
+var ePKHeXAfterStarter []byte
+//go:embed assets_test/e_pkhex_champ.sav
+var ePKHeXChamp []byte
